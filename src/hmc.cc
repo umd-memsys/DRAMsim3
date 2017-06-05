02b7ace68df15bdc2111a105596f73f0add75eaf
shavvn <tig314@gmail.com>
2017-06-05 00:01:37 -0400

    init commit for hmc...

---
+++ src/hmc.cc
@@ -0,0 +1,132 @@
+#include "hmc.h"
+
+using namespace std;
+using namespace dramcore;
+
+HMCRequest::HMCRequest(uint64_t req_id, HMCReqType req_type, uint64_t hex_addr_1, uint64_t hex_addr2, int operand_len):
+    req_id_(req_id),
+    type(req_type),
+    operand_1(hex_addr_1),
+    operand_2(hex_addr_2)
+{
+    switch (req_type) {
+        case HMCReqType::RD:
+            flits = 1;
+            resp_type = HMCRespType::RD_RS;
+            resp_flits = operand_len >> 4 + 1;
+            break;
+        case HMCReqType::WR:
+            flits = operand_len >> 4 + 1;
+            resp_type = HMCRespType::WR_RS;
+            resp_flits = 1;
+            break;
+        case HMCReqType::P_WR:
+            flits = operand_len >> 4 + 1;
+            resp_type = HMCRespType::NONE;
+            resp_flits = 0;
+            break;
+        case HMCReqType::2ADD8:
+        case HMCReqType::ADD16:
+            flits = 2;
+            resp_type = HMCRespType::WR_RS;
+            resp_flits = 1;
+            break;
+        case HMCReqType::P_2ADD8:
+        case HMCReqType::P_ADD16:
+            flits = 2;
+            resp_type = HMCRespType::NONE;
+            resp_flits = 0;
+            break;
+        case HMCReqType::2ADDS8R:
+        case HMCReqType::ADDS16R:
+            flits = 2;
+            resp_type = HMCRespType::RD_RS;
+            resp_flits = 2;
+            break;
+        case HMCReqType::INC8:
+            flits = 1;
+            resp_type = HMCRespType::WR_RS;
+            resp_flits = 1;
+            break;
+        case HMCReqType::P_INC8:
+            flits = 1;
+            resp_type = HMCRespType::NONE;
+            resp_flits = 0;
+            break;
+        case HMCReqType::XOR16:
+        case HMCReqType::OR16:
+        case HMCReqType::NOR16:
+        case HMCReqType::AND16:
+        case HMCReqType::NAND16:
+        case HMCReqType::CASGT:
+        case HMCReqType::CASLT:
+        case HMCReqType::CASEQ:
+            flits = 2;
+            resp_type = HMCRespType::RD_RS;
+            resp_flits = 2;
+            break;
+        case HMCReqType::EQ:
+        case HMCReqType::BWR:
+            flits = 2;
+            resp_type = HMCRespType::WR_RS;
+            resp_flits = 1;
+            break;
+        case HMCReqType::P_BWR:
+            flits = 2;
+            resp_type = HMCRespType::NONE;
+            resp_flits = 0;
+            break;
+        case HMCReqType::BWR8R:
+        case HMCReqType::SWAP16:
+            flits = 2;
+            resp_type = HMCRespType::RD_RS;
+            resp_flits = 2;
+            break;
+        default:
+            AbruptExit(__FILE__, __LINE__);
+            break;
+    }
+}
+
+
+HMCSystem::HMCSystem(const std::string &config_file, std::function<void(uint64_t)> callback):
+    callback_(callback),
+    logic_clk_(0),
+    dram_clk_(0)
+{
+    ptr_config_ = new Config(config_file);
+    ptr_timing_ = new Timing(*ptr_config_);
+    ptr_stats_ = new Statistics();
+
+    // initialize vaults and crossbar
+    // the first layer of xbar will be num_links * 4 (4 for quadrants)
+    // the second layer will be a 1:8 xbar
+    // (each quadrant has 8 vaults and each quadrant can access any ohter quadrant)
+
+
+    //Stats output files
+    stats_file_.open(ptr_config_->stats_file);
+    cummulative_stats_file_.open(ptr_config_->cummulative_stats_file);
+    epoch_stats_file_.open(ptr_config_->epoch_stats_file);
+    stats_file_csv_.open(ptr_config_->stats_file_csv);
+    cummulative_stats_file_csv_.open(ptr_config_->cummulative_stats_file_csv);
+    epoch_stats_file_csv_.open(ptr_config_->epoch_stats_file_csv);
+
+    ptr_stats_->PrintStatsCSVHeader(stats_file_csv_);
+    ptr_stats_->PrintStatsCSVHeader(cummulative_stats_file_csv_);
+    ptr_stats_->PrintStatsCSVHeader(epoch_stats_file_csv_);
+}
+
+
+HMCSystem::~HMCSystem() {
+    
+    delete(ptr_stats_);
+    delete(ptr_timing_);
+    delete(ptr_config_);
+    stats_file_.close();
+    cummulative_stats_file_.close();
+    epoch_stats_file_.close();
+    stats_file_csv_.close();
+    cummulative_stats_file_csv_.close();
+    epoch_stats_file_csv_.close();
+}
---
+++ src/hmc.h
@@ -0,0 +1,105 @@
+#ifndef __HMC_H
+#define __HMC_H
+
+#include <functional>
+#include "common.h"
+#include "configuration.h"
+
+
+namespace dramcore {
+
+
+enum class HMCReqType {
+    RD,
+    WR,
+    P_WR,
+    2ADD8,
+    ADD16,
+    P_2ADD8,
+    P_ADD16,
+    2ADDS8R,
+    ADDS16R,
+    INC8,
+    P_INC8,
+    XOR16,
+    OR16,
+    NOR16,
+    AND16,
+    NAND16,
+    CASGT,
+    CASLT,
+    CASEQ,
+    EQ,
+    BWR,
+    P_BWR,
+    BWR8R,
+    SWAP16,
+    SIZE
+};
+
+enum class HMCRespType {
+    NONE,
+    RD_RS,
+    WR_RS,
+    ERR,
+    SIZE
+};
+
+
+enum class HMCLinkType {
+    HOST_TO_DEV,
+    DEV_TO_DEV,
+    SIZE
+};
+
+
+class HMCRequest {
+public:
+    HMCRequest(uint64_t req_id, HMCReqType req_type, uint64_t hex_addr_1, uint64_t hex_addr2, int operand_len);
+    uint64_t req_id_;
+    HMCReqType type;
+    uint64_t operand_1,
+    uint64_t operand_2,
+    int operand_len_;
+    int flits;
+    HMCRespType resp_type;
+    int resp_flits;
+};
+
+class HMCLink {
+public:
+    HMCLink(const Config &config, std::function<void(uint64_t)>& callback);
+    std::function<void(uint64_t req_id)> callback_;
+private:
+
+};
+
+
+class HMCSystem {
+public:
+    HMCSystem(const std::string &config_file, std::function<void(uint64_t)> callback);
+    ~HMCSystem();
+    bool InserReq();
+    Config* ptr_config_;
+private:
+    uint64_t logic_clk_;
+    uint64_t dram_clk_;
+    uint64_t id_;
+    Timing* ptr_timing_;
+    Statistics* ptr_stats_;
+    std::vector<HMCRequest> buffer_q_;
+
+    //Stats output files
+    std::ofstream stats_file_;
+    std::ofstream cummulative_stats_file_;
+    std::ofstream epoch_stats_file_;
+    std::ofstream stats_file_csv_;
+    std::ofstream cummulative_stats_file_csv_;
+    std::ofstream epoch_stats_file_csv_;
+};
+
+
+}
+
+#endif
+
