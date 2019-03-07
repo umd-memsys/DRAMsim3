a728f4b3472ec8253004b06bce8c6bfc74d2e8c9
Shang Li <shangli@umd.edu>
2019-03-07 13:30:29 -0500

    Simplify CLI

--- src/main.cc
+++ src/main.cc
@@ -2,25 +2,26 @@
 #include "./../ext/headers/args.hxx"
 #include "cpu.h"
 
-// only exception of namespace as this will never collide with other namespaces
 using namespace dramsim3;
 
 int main(int argc, const char **argv) {
     args::ArgumentParser parser("DRAM Simulator.", "");
-    args::HelpFlag help(parser, "help", "Display the help menu", {"h", "help"});
-    args::ValueFlag<uint64_t> numb_cycles_arg(parser, "numb_cycles",
-                                              "Number of cycles to simulate",
-                                              {'n', "numb-cycles"}, 100000);
-    args::ValueFlag<std::string> config_arg(parser, "config", "The config file",
-                                            {'c', "config-file"});
+    args::HelpFlag help(parser, "help", "Display the help menu", {'h', "help"});
+    args::ValueFlag<uint64_t> num_cycles_arg(parser, "num_cycles",
+                                             "Number of cycles to simulate",
+                                             {'c', "cycles"}, 100000);
     args::ValueFlag<std::string> output_dir_arg(
-        parser, "output-dir", "Output directory for stats files",
+        parser, "output_dir", "Output directory for stats files",
         {'o', "output-dir"}, ".");
-    args::ValueFlag<std::string> cpu_arg(parser, "cpu-type",
-                                         "Type of cpu - random, trace, stream",
-                                         {"cpu-type"}, "random");
+    args::ValueFlag<std::string> pattern_arg(
+        parser, "pattern_type", "pattern generator - random, stream",
+        {'p', "pattern"}, "");
     args::ValueFlag<std::string> trace_file_arg(
-        parser, "trace", "The trace file", {"trace-file"});
+        parser, "trace",
+        "Trace file, setting this option will ignore -s option",
+        {'t', "trace"});
+    args::Positional<std::string> config_arg(
+        parser, "config", "The config file name (mandatory)");
 
     try {
         parser.ParseCLI(argc, argv);
@@ -33,24 +34,26 @@ int main(int argc, const char **argv) {
         return 1;
     }
 
-    uint64_t cycles = args::get(numb_cycles_arg);
-    std::string config_file, output_dir, trace_file, cpu_type;
-    config_file = args::get(config_arg);
-    output_dir = args::get(output_dir_arg);
-    trace_file = args::get(trace_file_arg);
-    cpu_type = args::get(cpu_arg);
+    std::string config_file = args::get(config_arg);
+    if (config_file.empty()) {
+        std::cerr << parser;
+        return 1;
+    }
+
+    uint64_t cycles = args::get(num_cycles_arg);
+    std::string output_dir = args::get(output_dir_arg);
+    std::string trace_file = args::get(trace_file_arg);
+    std::string pattern_type = args::get(pattern_arg);
 
     CPU *cpu;
-    if (cpu_type == "random") {
-        cpu = new RandomCPU(config_file, output_dir);
-    } else if (cpu_type == "trace") {
+    if (!trace_file.empty()) {
         cpu = new TraceBasedCPU(config_file, output_dir, trace_file);
-    } else if (cpu_type == "stream") {
-        cpu = new StreamCPU(config_file, output_dir);
     } else {
-        cpu = nullptr;
-        std::cerr << "Unknown cpu type" << std::endl;
-        AbruptExit(__FILE__, __LINE__);
+        if (pattern_type == "stream" || pattern_type == "s") {
+            cpu = new StreamCPU(config_file, output_dir);
+        } else {
+            cpu = new RandomCPU(config_file, output_dir);
+        }
     }
 
     for (uint64_t clk = 0; clk < cycles; clk++) {
